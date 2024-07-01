ce27a6290e6b8a9ec5afd0849e1f03fe728c63b8
Nông Văn Thắng <52882277+ThangStar@users.noreply.github.com>
2024-07-01 13:41:10 +0700

    main

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'thangnv2k2@gmail.com',
+    phone: '+84383253638',
+    address: 'Đắk Lắk - Việt Nam',
+    github: 'https://github.com/ThangStar',
+    facebook: 'https://www.facebook.com/mt.meow.meow/',
+    linkedIn: 'https://www.linkedin.com/in/th%E1%BA%AFng-star-11ab13276/',
+    twitter: 'https://twitter.com/',
+    stackOverflow: 'https://stackoverflow.com/',
+    devUsername: "thangnv"
+}
